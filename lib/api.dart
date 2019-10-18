745a8889db8bc58cf99a259699d97251bb3a0f9f
douglaspereira <douglas.pereira@serpro.gov.br>
2019-10-18 12:02:57 -0300

    projeto de busca videos youtube em flutter

---
+++ lib/api.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:fluttertube/models/video.dart';
+import 'package:http/http.dart' as http;
+
+const API_KEY = "AIzaSyBlm4W-0fprHnpscogRiMPTTROuBw4IflE";
+
+class Api {
+
+  search(String search) async {
+
+    http.Response response = await http.get(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+    );
+
+    decode(response);
+  }
+
+  List<Video> decode(http.Response response){
+
+    if(response.statusCode == 200){
+
+      var decoded = json.decode(response.body);
+
+      List<Video> videos = decoded["items"].map<Video>(
+          (map){
+            return Video.fromJson(map);
+          }
+      ).toList();
+
+      return videos;
+
+    } else {
+
+      throw Exception("Failed to load videos");
+    }
+
+  }
+}
